5efd7ead6d9de8c9f730c45a63c9ff0c2eb0ff07
adarshas04 <adarshrig@gmail.com>
2024-02-09 03:08:26 +0530

    refactored and added filtering

--- src/routes/taskActions.js
+++ src/routes/taskActions.js
@@ -5,91 +5,72 @@ const fs = require("fs");
 const path = require("path");
 const controller = require("../helpers/controller");
 
-taskActions.use(express.json())
+taskActions.use(express.json());
 
 taskActions.get("/", (req, res) => {
-    return res.status(200).json(taskData.tasks);
+    let filteredTasks = [...taskData.tasks];
+
+    // Filtering based on completion status
+    const { completed } = req.query;
+    console.log(completed)
+    if (completed) {
+        filteredTasks = filteredTasks.filter(task => task.completed === (completed === 'true'));
+    }
+
+    return res.status(200).json(filteredTasks);
 });
 
 taskActions.get("/:id", (req, res) => {
-    const tasks = taskData.tasks;
-    let filteredTask = tasks.filter((task) => {
-        return task.id == req.params.id;
-    });
-    if (filteredTask.length == 0) {
+    const taskId = parseInt(req.params.id);
+    const task = taskData.tasks.find((task) => task.id === taskId);
+    if (!task) {
         return res.status(404).send("Task not found");
-    } else {
-        return res.status(200).json(filteredTask[0]);
     }
+    return res.status(200).json(task);
 });
 
 taskActions.post("/", (req, res) => {
     const newTask = req.body;
-    let writePath = path.resolve(__dirname, "../../task.json");
-
     if (controller.validateTaskInfo(newTask, taskData).status) {
-        let allTasks = JSON.parse(JSON.stringify(taskData));
-        newTask.id = allTasks.tasks.length + 1;
-        allTasks.tasks.push(newTask);
+        // Generate and assign a new task ID
+        const maxId = taskData.tasks.reduce((max, task) => (task.id > max ? task.id : max), 0);
+        newTask.id = maxId + 1;
 
-        fs.writeFileSync(writePath, JSON.stringify(allTasks), {
-            encoding: "utf8",
-            flag: "w",
-        });
-        res.status(201).json(controller.validateTaskInfo(newTask, taskData));
+        taskData.tasks.push(newTask);
+        fs.writeFileSync(path.resolve(__dirname, "../../task.json"), JSON.stringify(taskData), { encoding: "utf8", flag: "w" });
+        res.status(201).json({ status: true, message: "Task has been created" });
     } else {
-        res.status(400).json(controller.validateTaskInfo(newTask, taskData));
+        res.status(400).json({ status: false, message: "Task data does not contain complete info. Please check your inputs" });
     }
 });
 
 taskActions.put("/:id", (req, res) => {
-    const newTask = req.body;
     const taskId = parseInt(req.params.id);
+    const newTask = req.body;
     newTask.id = taskId;
-    let writePath = path.resolve(__dirname, "../../task.json");
-    console.log(newTask);
-    if(!controller.validateTaskId(newTask, taskData)) {
-
+    if (!controller.validateTaskId(newTask, taskData)) {
         return res.status(404).send("Task not found");
-
-    }else if(controller.validateTaskInfoForUpdate(newTask, taskData).status) {
-
-        let allTasks = JSON.parse(JSON.stringify(taskData));
-        let taskIndex = allTasks.tasks.findIndex((task) => task.id === taskId)
-        allTasks.tasks[taskIndex] = newTask;
-
-        fs.writeFileSync(writePath, JSON.stringify(allTasks), {
-            encoding: "utf8",
-            flag: "w",
-        });
-
-        res.status(200).json(controller.validateTaskInfoForUpdate(newTask, taskData));
-
+    }
+    if (controller.validateTaskInfo(newTask, taskData).status) {
+        const taskIndex = taskData.tasks.findIndex((task) => task.id === taskId);
+        taskData.tasks[taskIndex] = newTask;
+        fs.writeFileSync(path.resolve(__dirname, "../../task.json"), JSON.stringify(taskData), { encoding: "utf8", flag: "w" });
+        res.status(200).json({ status: true, message: "Task has been updated" });
     } else {
-
         res.status(400).send("Invalid task update request");
-        
     }
 });
 
 taskActions.delete("/:id", (req, res) => {
     const taskId = parseInt(req.params.id);
-    let writePath = path.resolve(__dirname, "../../task.json");
-    let allTasks = JSON.parse(JSON.stringify(taskData));
-
-    const taskIndex = allTasks.tasks.findIndex(task => task.id === taskId);
+    const taskIndex = taskData.tasks.findIndex((task) => task.id === taskId);
     if (taskIndex !== -1) {
-        allTasks.tasks.splice(taskIndex, 1);
-        fs.writeFileSync(writePath, JSON.stringify(allTasks), {
-            encoding: "utf8",
-            flag: "w",
-        });
-
+        taskData.tasks.splice(taskIndex, 1);
+        fs.writeFileSync(path.resolve(__dirname, "../../task.json"), JSON.stringify(taskData), { encoding: "utf8", flag: "w" });
         res.status(200).json({ message: 'Task deleted successfully' });
     } else {
         res.status(404).json({ message: 'Task not found' });
     }
-
-})
+});
 
 module.exports = taskActions;
